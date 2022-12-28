f66c8c4e44d491eaed6eaa3ce7412c53b2a5e231
vkats90 <mars77@gmail.com>
2022-12-28 14:39:33 -0700

    feat: added auth strategy, serialization and error  handling

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
-
+const { ObjectID } = require('mongodb');
+const LocalStrategy = require('passport-local');
 
 const app = express();
 
@@ -26,11 +27,37 @@ app.use(passport.session())
 app.set('view engine','pug');
 app.set('views','./views/pug');
 
-app.route('/').get((req, res) => {
-  res.render('index',{title:'Hello',message:'Please log in'})
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+
+  passport.use(new LocalStrategy((username, password, done) => {
+    myDataBase.findOne({ username: username }, (err, user) => {
+      console.log(`User ${username} attempted to log in.`);
+      if (err) return done(err);
+      if (!user) return done(null, false);
+      if (password !== user.password) return done(null, false);
+      return done(null, user);
+    });
+  }));
+
+  app.route('/').get((req, res) => {
+    res.render('index',{title:'Connected to Database',message:'Please login',showLogin: true})
+  });
+
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('index', { title: e, message: 'Unable to connect to database' });
+  });
 });
-
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
